63860da8e10f46de4fdb6da0aa4b941235489d0f
sharmilaperiyasamy <sharmilaperiyasamy1992@gmail.com>
2022-07-14 13:37:19 +0530

    [Sharmila] uc1-testcase1.2 invalid file

--- StateAnalyzerTest/TestStateCensusData.cs
+++ StateAnalyzerTest/TestStateCensusData.cs
@@ -3,12 +3,28 @@ namespace StateAnalyzerTest
     public class Tests
     {
         [Test]
+        //uc1 testcase1.1
         public void Census_Analyzer()
         {
             int expected = 29;
             Indian_Census_Analyzer.StateAnalyser state = new Indian_Census_Analyzer.StateAnalyser();
             int output = state.DataAnalyzer(@"C:\Users\Lenovo\source\repos\Bridgelabz\Indian_State_Census_Analyzer\StateCensusData.csv");
             Assert.AreEqual(expected, output);
+        }
+        //uc1 testcase1.2
+        public void Census_AnalyzerException()
+        {
+            try
+            {
+                int expected = 29;
+                Indian_Census_Analyzer.StateAnalyser state = new Indian_Census_Analyzer.StateAnalyser();
+                int output = state.DataAnalyzer(@"C:\Users\Lenovo\source\repos\Bridgelabz\Indian_State_Census_Analyzer\StateData.csv");
+                Assert.AreEqual(expected, output);
+            }
+                 catch (Exception ex)
+            {
+                Assert.AreEqual("Invalid File", ex.Message);
+            }
         }
     }
 }
